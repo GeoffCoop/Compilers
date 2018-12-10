e421fc7e0252b877072b4742b21b73145cd62472
geoffcoop <kylegcooper@gmail.com>
2018-12-10 15:01:44 -0700

    string table and slight parser modifications

---
+++ Code/StringTable.cpp
@@ -0,0 +1,24 @@
+#include <StringTable.hpp>
+
+#include <string>
+#include <map>
+
+int label() {
+    static int count = 0;
+    return count++;
+}
+
+int StringTable::addString(std::string s){
+    auto f = s_table.find(s);
+    if (f == s_table.end()) {
+        return f->second;
+    }
+    int i = label();
+    s_table[i] = s;
+    id_table[s] = i;
+    return i;
+}
+
+std::string StringTable::lookupString(int i){
+    return id_table[i];
+}
---
+++ Code/StringTable.hpp
@@ -0,0 +1,23 @@
+#include <map>
+#include <string>
+#include <memory>
+
+
+class StringTable {
+    public:
+    StringTable() {}
+    static std::shared_ptr<StringTable> instance() {
+        if (!singleton){
+            singleton = std::shared_ptr<StringTable>(new StringTable);
+        }
+        return singleton;
+    }
+
+    int addString(std::string);
+    std::string lookupString(int);
+    std::map<std::string, int> getTable() { return s_table; }
+private:
+    static std::shared_ptr<StringTable> singleton;
+    std::map<std::string, int> s_table;
+    std::map<int, std::string> id_table;
+};
