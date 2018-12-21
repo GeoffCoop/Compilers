6b5e63bebe42375772cd1715b4133eea0790da16
geoffcoop <kylegcooper@gmail.com>
2018-12-20 20:38:05 -0700

    rearrange expressions for write stmt

--- Code/Expressions/Expression.hpp
+++ Code/Expressions/Expression.hpp
@@ -1,9 +1,15 @@
 #ifndef EXPRESSION_HPP
 #define EXPRESSION_HPP
 
-class Expression
-{
+#include "../Type.hpp"
+#include <memory>
 
+class Expression 
+{
+public:
+    Expression(std::shared_ptr<Type> t, int r): reg(r), type(t) {}
+    std::shared_ptr<Type> type;
+    int reg;
 };
 
 #endif
